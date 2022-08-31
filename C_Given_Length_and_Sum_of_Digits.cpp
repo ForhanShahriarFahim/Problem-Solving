90b8c687a0321476977cbf67e8b43c7818e8c169
Fahim <dfahim432@gmail.com>
2022-08-31 16:49:34 +0600

    Codeforces

---
+++ C_Given_Length_and_Sum_of_Digits.cpp
@@ -0,0 +1,142 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/detail/standard_policies.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+//**************************************************************************************************************************************************************************
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+//**************************************************************************************************************************************************************************
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define mod 1000000007
+#define pi acos(-1.0)
+#define eps 1e-9
+#define inf 1e18
+#define sz(x) (int)((x).size())
+#define gcd(a, b) __gcd(a, b)
+#define LCM(x, y) (((x) / __gcd((x), (y))) * (y))
+#define mem(x, n) memset(x, n, sizeof(x))
+#define setbits(x) __builtin_popcountll(x)
+#define zrobits(x) __builtin_ctzll(x)
+#define ps(x, y) fixed << setprecision(y) << x
+#define All(x) (x).begin(), (x).end()
+//**************************************************************************************************************************************************************************
+char gap = 32;
+template <typename T>
+ostream &operator<<(ostream &os, const vector<T> &v)
+{
+    os << '{';
+    for (const auto &x : v)
+        os << gap << x;
+    return os << '}';
+}
+template <typename A, typename B>
+ostream &operator<<(ostream &os, const pair<A, B> &p)
+{
+    return os << '(' << p.first << gap << p.second << ')';
+}
+
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T)
+{
+    cerr << ' ' << H;
+    dbg_out(T...);
+}
+
+#ifndef ONLINE_JUDGE
+#define dbg(...) cerr << '(' << #__VA_ARGS__ << ')' << ':', dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+typedef tree<int, null_type, less_equal<int>,
+             rb_tree_tag,
+             tree_order_statistics_node_update>
+    ordered_set;
+//*************************************************************************************************************************************************************************
+
+void c_p_c()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+void solve()
+{
+    int no, sum;
+    cin>>no>>sum;
+    if(no == 1 && sum < 10)
+    {
+        cout<<sum<<" "<<sum;
+    }
+    else if(no * 9 < sum || !sum)
+    {
+        cout<<-1<<" "<< -1;
+    }
+    else 
+    {
+        vector<int> small(no, 0);
+        small[0] = 1;
+        int temp = sum - 1;
+        for(int i = no-1;i>0;i--)
+        {
+            if(temp<9)
+            {
+                small[i] = temp;
+                temp-=temp;
+            }
+            else 
+            {
+                small[i] = 9;
+                temp-=9;
+            }
+        }
+        small[0]+=temp;
+        for(int i = 0;i<no;i++)
+        {
+            cout<<small[i];
+        }
+        cout<<" ";
+        for(int i = 0;i<no;i++)
+        {
+            if(sum<9)
+            {
+                cout<<sum;
+                sum-=sum;
+            }
+            else 
+            {
+                cout<<9;
+                sum-=9;
+            }
+        }
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    // c_p_c();
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+#ifndef ONLINE_JUDGE
+#endif
+    int t = 1;
+    //cin >> t;
+    for (int i = 0; i < t; i++)
+        solve();
+    cerr << "Time : " << 1000 * ((double)clock()) / (double)CLOCKS_PER_SEC << "ms\n";
+    return 0;
+}
